053f82345e296e04fda0dec154e8b7ed8fccf9c4
Kaleb Kreft <kkreft@gmail.com>
2025-06-12 12:38:01 -0700

    Info2Go v.1 Bugfix.5

--- nfo/js/app.js
+++ nfo/js/app.js
@@ -28,7 +28,7 @@ const app = {
 
     registerServiceWorker: () => {
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/sw.js')
+            navigator.serviceWorker.register('./sw.js')
                 .then(registration => console.log('Service Worker registered with scope:', registration.scope))
                 .catch(error => console.error('Service Worker registration failed:', error));
         }
