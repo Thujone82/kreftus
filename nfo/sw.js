bd841d3ae74442a5b0ef36b784cff6d63e5b070b
Kaleb Kreft <kkreft@gmail.com>
2025-06-15 07:39:12 -0700

    nfo2go v.1 Cache.Refresh.Tuning.4

--- nfo/sw.js
+++ nfo/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'info2go-v1-cache'; // KKREFT 061424@2237#
+const CACHE_NAME = 'info2go-061424@2237-cache'; // KKREFT 
 const SW_CONSTANTS = { // Defined here as sw.js doesn't import app.js
     SW_MESSAGES: {
         SKIP_WAITING: 'SKIP_WAITING'
