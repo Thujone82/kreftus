e329e77d0a9343737b8cdeb9b213487da2a8100b
Kaleb Kreft <kkreft@gmail.com>
2025-06-17 10:43:42 -0700

    BTC Track Formatting Update.2

--- btc/sw.js
+++ btc/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tc-track-cache-v1-0617@1040'; // Ensure this is updated with current MMDD@HHMM
+const CACHE_NAME = 'tc-track-cache-v1-0617@1043'; // Ensure this is updated with current MMDD@HHMM
 const urlsToCache = [
     './',
     './index.html',
