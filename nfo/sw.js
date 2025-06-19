34f36169269b78683ce08a4d94a531da0a10cc1f
Kaleb Kreft <kkreft@gmail.com>
2025-06-18 22:26:44 -0700

    nfo2go v.2 Weather Refresh for Info Modal.2

--- nfo/sw.js
+++ nfo/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'info2go-v2-061825@2211-cache'; // Updated cache name for v.2
+const CACHE_NAME = 'info2go-v2-061825@2225-cache'; // Updated cache name for v.2
 const SW_CONSTANTS = { // Defined here as sw.js doesn't import app.js
     SW_MESSAGES: {
         SKIP_WAITING: 'SKIP_WAITING'
