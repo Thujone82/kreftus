68f09ee407bfe59d6e6c748529374ab7ecccd40a
Kaleb Kreft <kkreft@gmail.com>
2025-06-25 12:34:16 -0700

    SpiroGen v2.4 - ZoomFraming

--- spiro/sw.js
+++ spiro/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'spirograph-generator-v2-062525@1216'; 
+const CACHE_NAME = 'spirograph-generator-v2-062525@1233'; 
 const urlsToCache = [
   './', // For accessing the root
   './index.html',
