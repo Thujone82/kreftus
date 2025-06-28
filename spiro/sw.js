974a2d4a4264b79ddba6d98c4c4c6b223db0ac12
Kaleb Kreft <kkreft@gmail.com>
2025-06-28 15:12:19 -0700

    SpiroGen v2.5 - precision

--- spiro/sw.js
+++ spiro/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'spirograph-generator-v2-062825@1119'; 
+const CACHE_NAME = 'spirograph-generator-v2-062825@1310'; 
 const urlsToCache = [
   './', // For accessing the root
   './index.html',
