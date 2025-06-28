61278eaf90b24b34584d74afd95fec177c3268f4
Kaleb Kreft <kkreft@gmail.com>
2025-06-28 10:32:15 -0700

    feat(spiro): add 0.2 to randomFracOptions and set total rotations based on it.2

--- spiro/sw.js
+++ spiro/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'spirograph-generator-v2-062825@1026'; 
+const CACHE_NAME = 'spirograph-generator-v2-062825@1032'; 
 const urlsToCache = [
   './', // For accessing the root
   './index.html',
