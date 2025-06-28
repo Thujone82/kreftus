4a5c93ecfe02517462d05cf740e97868a87fa7bd
Kaleb Kreft <kkreft@gmail.com>
2025-06-28 10:23:33 -0700

    feat(spiro): update sw.js cache name

--- spiro/sw.js
+++ spiro/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'spirograph-generator-v2-062525@1659'; 
+const CACHE_NAME = 'spirograph-generator-v2-062825@1026'; 
 const urlsToCache = [
   './', // For accessing the root
   './index.html',
