6bf777cc48249e8347b0b22de276df9519256b68
Kaleb Kreft <kkreft@gmail.com>
2025-07-04 19:28:01 -0700

    vBTC v1.1 PS + build script [PS2EXE]

---
+++ ps/vbtc/build.ps1
@@ -0,0 +1,13 @@
+<#
+    This build script uses PS2EXE to conver vBTC PowerShell script into an executable.
+#>
+Write-Host "vbtc.ps1 -> vbtc.exe..." -ForegroundColor Cyan
+Invoke-ps2exe "vbtc.ps1" "vbtc.exe" `
+    -iconFile "../../btc/icons/bitcoin_small.ico" `
+    -title "vBTC - Virtual Bitcoin Trading Simulator" `
+    -description "An interactive PowerShell-based Bitcoin trading application." `
+    -product "vBTC" `
+    -company "kreft.us" `
+    -copyright "Copyright (c) 2025" `
+    -version "1.1.0.0"
+Write-Host "Build complete!" -ForegroundColor Green
