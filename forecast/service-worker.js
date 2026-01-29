b197a9972443d839378396076ba4a0b1063ce45e
Kaleb Kreft <kkreft@gmail.com>
2026-01-29 08:13:32 -0800

    Update manifest to correct icon.

--- forecast/service-worker.js
+++ forecast/service-worker.js
@@ -1,4 +1,4 @@
-﻿let CACHE_NAME = 'forecast-v1.0.0-012226@1956';
+﻿let CACHE_NAME = 'forecast-v1.0.0-012926@0812';
 let STATIC_CACHE = 'forecast-static-v1.0.0';
 let DATA_CACHE = 'forecast-data-v1.0.0';
 
